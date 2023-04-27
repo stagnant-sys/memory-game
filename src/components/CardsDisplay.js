47793497906741377a410fb7b0ca041d6c6c8264
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-04-27 16:35:56 +0200

    Add randomize cards order after each winning click
    To do :
    - Score update
    - Game over message

--- src/components/CardsDisplay.js
+++ src/components/CardsDisplay.js
@@ -4,20 +4,20 @@ import { click } from "@testing-library/user-event/dist/click";
 
 export default function CardsDisplay () {
   const [cards, setCards] = useState(
-    {
-      tomberry: {
+    [
+      {
         id: 'tomberry',
         src: "tomberry.jpg",
         },
-      mog: {
+      {
         id: 'mog',
         src: "mog.jpg",
       },
-      chocobo: {
+      {
         id: 'chocobo',
         src: "chocobo.jpg"
       }
-  });
+    ]);
 
   const [clickedCards, setClickedCards] = useState([]);
 
@@ -26,20 +26,33 @@ export default function CardsDisplay () {
       console.log('You lose');
     } else {
       setClickedCards([...clickedCards, id]);
+      randomizeCards(cards);
       // score +1
       console.log(`Add ${id} to clickedCards`);
     }
   };
 
-  const sendScore = () => {
 
+
+  const randomizeCards = (array) => {
+    let tempArray = [...array];
+    let currentIndex = tempArray.length,  randomIndex;
+    
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // Swap it with the current element.
+      [tempArray[currentIndex], tempArray[randomIndex]] = [tempArray[randomIndex], tempArray[currentIndex]];
+      }
+      setCards(tempArray);
   };
 
   return (
     <div className="cards-display">
-      <Card src={cards.chocobo.src} id={cards.chocobo.id} callbackFromParent={getClickedCard} />
-      <Card src={cards.tomberry.src} id={cards.tomberry.id} callbackFromParent={getClickedCard} />
-      <Card src={cards.mog.src} id={cards.mog.id} callbackFromParent={getClickedCard} />
+      <Card src={cards[0].src} id={cards[0].id} callbackFromParent={getClickedCard} />
+      <Card src={cards[1].src} id={cards[1].id} callbackFromParent={getClickedCard} />
+      <Card src={cards[2].src} id={cards[2].id} callbackFromParent={getClickedCard} />
 
       <ul>
 
